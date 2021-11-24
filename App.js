637f8bdc7b2cf0f53dc3e6f2b1df83379d4dbb24
Harshitha <93976539+Harshutha@users.noreply.github.com>
2021-11-24 10:33:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello this is my first React app </h1>
+     <h2>I am learning React.js</h2>
+    </div>
+  );
+}
+
+export default App;
